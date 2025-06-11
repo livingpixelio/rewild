853fee52357f13880c70a66ea5a8a4d5c42a2cf2
Casey Ydenberg <hi@caydenberg.io>
2025-06-10 19:49:31 -0700

    Remove .obsidian dir from source

--- dev.ts
+++ dev.ts
@@ -5,10 +5,12 @@ import config from "./fresh.config.ts";
 import "$std/dotenv/load.ts";
 import { contentBuilder } from "foblog";
 
-await contentBuilder.build();
-contentBuilder.watch((digest, watcher) => {
-  Deno.writeTextFile("digest.txt", digest);
-  watcher.close();
-});
+if (!Deno.args.includes("build")) {
+  await contentBuilder.build();
+  contentBuilder.watch((digest, watcher) => {
+    Deno.writeTextFile("digest.txt", digest);
+    watcher.close();
+  });
+}
 
 await dev(import.meta.url, "./main.ts", config);
