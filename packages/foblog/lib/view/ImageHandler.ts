be7126da12f6cc99676ef2bcb9c384496020e4db
Casey Ydenberg <hi@caydenberg.io>
2025-07-11 11:57:02 -0700

    Rewrite ImageHandler to not track own sizes

--- packages/foblog/lib/view/ImageHandler.ts
+++ packages/foblog/lib/view/ImageHandler.ts
@@ -7,6 +7,7 @@ import { parseQuery } from "../../parsers/index.ts";
 import { Repository } from "../../storage/db.ts";
 import { getAttachmentPath, getContentPath } from "../../storage/disk.ts";
 import { image } from "../model/index.ts";
+import { config } from "../../plugin/config.ts";
 
 interface ImageParams {
   slug: string;
@@ -60,13 +61,15 @@ export const ImageHandler = (
       return new HttpError(404).toHttp();
     }
 
-    const sizes = imageData.sizes.slice().sort((a, b) => a.size - b.size);
+    const getFilename = (size: number) =>
+      `${imageData.slug}_${size}${imageData.format}`;
+    const sizes = config.images.sizes.slice().sort((a, b) => a - b);
     const size = typeof params.width === "number"
-      ? sizes.find((size) => size.size >= (params.width as number))
+      ? sizes.find((size) => size >= (params.width as number))
       : null;
 
     const attachmentPath = size
-      ? getAttachmentPath(size?.filename, context)
+      ? getAttachmentPath(getFilename(size), context)
       : null;
 
     if (attachmentPath && await exists(attachmentPath)) {
